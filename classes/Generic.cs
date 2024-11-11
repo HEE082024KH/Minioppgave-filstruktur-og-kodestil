a40df266db637b9b22bb8bf66e03eea876c45c43
hee082024kh <hee082024@gmail.com>
2024-11-11 12:43:08 +0100

    Created GenericStuff method

--- classes/Generic.cs
+++ classes/Generic.cs
@@ -4,4 +4,9 @@ public class Generic
   public Generic generic2;
   public string something;
   public double s0meth1ng;
+
+  public void GenericStuff()
+  {
+    Console.WriteLine($"Generic1: {generic} \nGeneric2:{generic2} \nString:{something} \nDouble:{s0meth1ng}");
+  }
 }
